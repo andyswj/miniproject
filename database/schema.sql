79e769b60a83902c01c2f3cdab5988ecec05cc74
andyswj <e0696682@u.nus.edu>
2022-05-07 17:18:54 +0800

    first commit

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+DROP SCHEMA IF EXISTS miniproject;
+
+CREATE SCHEMA miniproject;
+
+USE miniproject;
+
+
+CREATE TABLE user(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(64) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE module_review(
+    review_id INT NOT NULL AUTO_INCREMENT,
+    module_name VARCHAR(256) NOT NULL,
+    rating INT,
+    comment VARCHAR(256),
+    user_id INT NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (review_id),
+    CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES user(user_id)
+);
